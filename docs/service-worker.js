60af99ef930b309295a799fcc0133c386361f90d
uemill <good3.choco@gmail.com>
2020-06-19 14:55:13 +0900

    PWA対応

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,24 +1,37 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+var CACHE_NAME = 'pwa-caches::v2';
+var urlsToCache = [
+    'https://uemill.github.io/CFcalc/index.html',
+    'https://uemill.github.io/CFcalc/CFcalc.js'
+];
 
-self.addEventListener('install', function(e) {
-  	console.log('[ServiceWorker] Install');
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', function(e) {
-  	console.log('[ServiceWorker] Activate');
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
 });
 
-self.addEventListener('fetch', function(e) {
-	console.log('[ServiceWorker] fetch');
-});
-
-workbox.precaching.precacheAndRoute([
-  {
-    url: 'index.html',
-    revision: '10000'
-  },
-  {
-    url: 'CFcalc.js',
-    revision: '10000'
-  } 
-])
+self.addEventListener('activate',function(event){
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => {
+                if(CACHE_NAME.indexOf(key) === -1){
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+})
